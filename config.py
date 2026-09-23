643c59baef3e5e2c8020493b76bdc4b74b945de1
FatimaBatool514 <fbatool819@gmail.com>
2026-09-23 22:44:36 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+MODEL_NAME = "openai/gpt-oss-120b"
+
+
+if not GROQ_API_KEY:
+    raise ValueError(
+        "GROQ_API_KEY is not set."
+    )
